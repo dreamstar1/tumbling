f4f4e6e75a86a635d34435415e67f72c3020cb57
Song Simon Joon-Hee <g2junhee@cdf.toronto.edu>
2013-03-12 23:20:46 -0400

    create new DELETE for likes

--- a2drop.sql
+++ a2drop.sql
@@ -1,5 +1,6 @@
 -- drop all tables in database
-drop table if exists blog cascade;
-drop table if exists post cascade;
-drop table if exists image cascade;
+drop table if exists likes cascade;
 drop table if exists time_stamp cascade;
+drop table if exists image cascade;
+drop table if exists post cascade;
+drop table if exists blog cascade;
