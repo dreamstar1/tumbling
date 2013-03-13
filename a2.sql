ffdea8f9f41f5365a710ffd7367b754c71773c41
Song Simon Joon-Hee <g2junhee@cdf.toronto.edu>
2013-03-12 23:20:27 -0400

    created new database table

--- a2.sql
+++ a2.sql
@@ -9,25 +9,29 @@ create table post (
 	blog_url varchar(50) not null,
 	txt varchar(50),
 	img varchar(50),
-	dt timestamp not null,
-	last_track date not null,
-	note_count integer not null
+	dt timestamp not null
 );
 
 -- store image and post associated with it
 create table image (
-  url varchar(50),
-  post varchar(50) references post(url)
+	url varchar(50),
+	post varchar(50) references post(url)
 );
 
 -- time stamp associated with posts
 create table time_stamp (
-  id integer auto_increment primary key, 
-  ts date,
-  url varchar(50) references post(url),
-  seq integer,
-  inc integer,
-  cnt integer
+	id integer auto_increment primary key, 
+	ts timestamp,
+	url varchar(50) references post(url),
+	seq integer,
+	inc integer,
+	cnt integer
 );
 
+create table likes (
+	url varchar(50) not null,
+	person varchar(50) not null,
+	foreign key(url) references post(url),
+	foreign key(person) references blog(url)
+);
 
