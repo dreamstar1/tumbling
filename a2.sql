30013eb6ad652cd6800c7f755aef27577cb32912
Song Simon Joon-Hee <g2junhee@cdf.toronto.edu>
2013-03-13 21:54:55 -0400

    editted

--- a2.sql
+++ a2.sql
@@ -1,35 +1,36 @@
 -- blogs we are tracking
 create table blog (
-	url varchar(50) primary key
+	url varchar(500) primary key
 );
 
 -- posts we are tracking
 create table post (
-	url varchar(50) primary key,
-	txt varchar(50),
-	img varchar(50),
+	url varchar(500) primary key,
+	txt varchar(500),
+	img varchar(500),
 	dt timestamp not null
 );
 
 -- store image and post associated with it
 create table image (
-	url varchar(50),
-	post varchar(50) references post(url)
+	url varchar(500),
+	post varchar(500) references post(url)
 );
 
 -- time stamp associated with posts
 create table time_stamp (
 	id integer auto_increment primary key, 
 	ts timestamp,
-	url varchar(50) references post(url),
+	url varchar(500) references post(url),
 	seq integer,
 	inc integer,
 	cnt integer
 );
 
 create table likes (
-	url varchar(50) not null,
-	person varchar(50) not null,
+	url varchar(500) not null,
+	person varchar(500) not null,
+	primary key(url, person),
 	foreign key(url) references post(url),
 	foreign key(person) references blog(url)
 );
